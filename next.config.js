5a7bd9545af1f772e08eebb95bc1e8b05bd3088d
jolow99 <jolow999@gmail.com>
2024-05-27 21:22:21 +0800

    Edited api configuration

--- next.config.js
+++ next.config.js
@@ -22,21 +22,7 @@ const config = {
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
-      },
+      }
     ];
   },
 };

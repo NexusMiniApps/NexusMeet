fd3779623fe6c6a725479971539bd3d1a0277182
jolow99 <jolow999@gmail.com>
2024-05-28 17:33:55 +0800

    Remove fastapi files

--- next.config.js
+++ next.config.js
@@ -14,17 +14,6 @@ const config = {
 
     return config;
   },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      }
-    ];
-  },
 };
 
 export default config;

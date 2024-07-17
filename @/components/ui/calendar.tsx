74d78e77f1bfe45591b67005ca94373003c160c0
jolow99 <jolow999@gmail.com>
2024-07-17 15:15:53 +0800

    Disable selection of dates before today

--- @/components/ui/calendar.tsx
+++ @/components/ui/calendar.tsx
@@ -15,9 +15,13 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
+
+  const today = new Date();
+
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
+      disabled = {{ before: today }}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
